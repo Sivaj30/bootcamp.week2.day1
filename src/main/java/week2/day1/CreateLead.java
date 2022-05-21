948dc27dd0997de30630ed9af3eb08ff658098ee
Sivaj30 <1895Siva@gmail.com>
2022-05-21 17:02:22 +0530

    commit message

---
+++ src/main/java/week2/day1/CreateLead.java
@@ -0,0 +1,21 @@
+package week2.day1;
+
+
+
+
+
+
+public class CreateLead  {
+	
+    public void createLead() {
+		// TODO Auto-generated method stub
+		
+		
+		
+		System.out.println("Sample file to push the code ");
+		
+
+	}
+
+}
+
